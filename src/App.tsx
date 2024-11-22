e4ee97651ca1cb792e6cbee1fb03a3a187bbc7ef
jearthman <james.earthman@gmail.com>
2024-11-22 08:37:09 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Button from './design-system/Button';
+
+function App() {
+  return (
+    <>
+      <div className="component-row">
+        <Button label="Button" />
+      </div>
+    </>
+  );
+}
+
+export default App;
